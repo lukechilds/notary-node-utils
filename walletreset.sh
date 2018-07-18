5a5520a48088442b00fd8d247bd10efffc8f9d37
Luke Childs <lukechilds123@gmail.com>
2018-07-18 08:12:41 +0700

    Cleanup variables

--- walletreset.sh
+++ walletreset.sh
@@ -1,43 +1,55 @@
 #!/bin/bash
-#Config area
-komodo_data=~/.komodo                           #default would be ~/.komodo
-komodopath=komodo-cli                           #path to komodo-cli
-komododpath=komodod                             #path to komodod
-NNaddress=RPxsaGNqTKzPnbm5q7QXwu7b6EZWuLxJG3    #Your NN address key goes here
-source ~/komodo/src/pubkey.txt                  #path to your pubkey.txt
-args="-gen -genproclimit=1"                     #Any special args you pass komodod on start
-DATE=`date +%Y-%m-%d:%H:%M:%S`                  #get todays date for your wallet.dat backup file
-currdir=$(echo $PWD)                            #get the working directory
+
+# Coin we're resetting
+coin=KMD
+# daemon data directory where wallet.dat exists
+# e.g ~/.komodo
+data_dir=~/.komodo
+# Path to daemon
+# e.g komodod
+daemon=komodod
+# Path to daemon cli
+# e.g komodo-cli
+cli=komodo-cli
+# Address containing all your funds
+nn_address=RPxsaGNqTKzPnbm5q7QXwu7b6EZWuLxJG3
+# Arguments to pass to daemon on restart
+daemon_args="-gen -genproclimit=1"
+
+source ~/komodo/src/pubkey.txt
+DATE=$(date +%Y-%m-%d:%H:%M:%S)
+current_dir=$(echo $PWD)
 
 waitforconfirm () {
   sleep 15
   confirmations=0
   while [[ $confirmations -lt 1 ]]; do
-    confirmations=$($komodopath gettransaction $1 | jq -r .confirmations)
+    confirmations=$($cli gettransaction $1 | jq -r .confirmations)
     sleep 10
   done
 }
 
-echo "[KMD] Get new address and privkey for it, best to use new one each time to avoid bloating wallet."
-NewAddress=$($komodopath getnewaddress)
-NewPrivKey=$($komodopath dumpprivkey $NewAddress)
+echo "[$coin] Get new address and privkey for it, best to use new one each time to avoid bloating wallet."
+new_address=$($cli getnewaddress)
+new_privkey=$($cli dumpprivkey $new_address)
 
-echo "[KMD] Save the NN privkey to a variable so we can import it later."
-NNprivkey=$($komodopath dumpprivkey $NNaddress)
+echo "[$coin] Save the NN privkey to a variable so we can import it later."
+NNprivkey=$($cli dumpprivkey $nn_address)
 
-echo "[KMD] Add the new privkey to a file, incase something goes wrong we can import it to recover funds"
-echo $NewPrivKey >> NewAddressPrivKey
+echo "[$coin] Add the new privkey to a file, incase something goes wrong we can import it to recover funds"
+echo $new_privkey >> new_addressPrivKey
 
-echo "[KMD] Send entire balance to the new adress"
-TXID=$($komodopath sendtoaddress $NewAddress $($komodopath getbalance) "" "" true)
+echo "[$coin] Send entire balance to the new adress"
+txid=$($cli sendtoaddress $new_address $($cli getbalance) "" "" true)
+echo "[$coin] $txid"
 
-echo "[KMD] Check for confirmation of received funds"
-waitforconfirm $TXID
+echo "[$coin] Check for confirmation of received funds"
+waitforconfirm $txid
 
-echo "[KMD] stop the deamon"
-$komodopath stop
+echo "[$coin] stop the deamon"
+$cli stop
 
-echo "[KMD] wait for deamon to stop"
+echo "[$coin] wait for deamon to stop"
 stopped=0
 while [[ $stopped -eq 0 ]]; do
   sleep 10
@@ -48,33 +60,34 @@ while [[ $stopped -eq 0 ]]; do
   fi
 done
 
-echo "[KMD] move your old wallet, then return to our working directory"
-cd $komodo_data
+echo "[$coin] move your old wallet, then return to our working directory"
+cd $data_dir
 mv wallet.dat wallet.dat.$DATE.bak
-cd $currdir
+cd $current_dir
 
-echo "[KMD] restart the komodo deamon, it will generate a new empty wallet.dat on start"
-$komododpath -notary -pubkey=$pubkey $args > /dev/null 2>&1 &
+echo "[$coin] restart the komodo deamon, it will generate a new empty wallet.dat on start"
+$daemon -notary -pubkey=$pubkey $daemon_args > /dev/null 2>&1 &
 
-echo "[KMD] wait for deamon to start"
+echo "[$coin] wait for deamon to start"
 started=0
 while [[ $started -eq 0 ]]; do
   sleep 15
-  $komodopath getbalance > /dev/null 2>&1
+  $cli getbalance > /dev/null 2>&1
   outcome=$(echo $?)
   if [[ $outcome -eq 0 ]]; then
     started=1
   fi
 done
 
-echo "[KMD] import the private keys, we rescan for new address but not for NN address"
-$komodopath importprivkey $NewPrivKey
-$komodopath importprivkey $NNprivkey "" false
+echo "[$coin] import the private keys, we rescan for new address but not for NN address"
+$cli importprivkey $new_privkey
+$cli importprivkey $NNprivkey "" false
 
-echo "[KMD] Send the entire balance to the NN address"
-TXID=$($komodopath sendtoaddress $NNaddress $($komodopath getbalance) "" "" true)
+echo "[$coin] Send the entire balance to the NN address"
+txid=$($cli sendtoaddress $nn_address $($cli getbalance) "" "" true)
+echo "[$coin] $txid"
 
-echo "[KMD] Check for confirmation of received funds"
-waitforconfirm $TXID
+echo "[$coin] Check for confirmation of received funds"
+waitforconfirm $txid
 
-echo "[KMD] Process complete... please launch m_notary and carry out acsplit KMD"
+echo "[$coin] Process complete... please launch m_notary and carry out acsplit KMD"
