b393f54eb4fd14e98521fe8b018430cd903cffb7
Luke Childs <lukechilds123@gmail.com>
2018-07-22 13:52:03 +0100

    Fix typo calling splitfunds.sh in walletreset.sh

--- walletreset.sh
+++ walletreset.sh
@@ -101,7 +101,7 @@ waitforconfirm ${txid}
 echo "[${coin}] Returned funds confirmed"
 
 echo "[${coin}] Splitting out 100 UTXOs"
-txid=$(./splitfunds ${coin} 100 | jq -r .txid)
+txid=$(./splitfunds.sh ${coin} 100 | jq -r .txid)
 echo "[${coin}] Split broadcast: ${txid}"
 
 echo "[${coin}] Waiting for confirmation of UTXO split"
