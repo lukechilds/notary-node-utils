ae561b2a2c8e32d9a48614ae90957b584ce1c3ce
Luke Childs <lukechilds123@gmail.com>
2018-07-27 09:51:37 +0100

    Add upgrade script

---
+++ upgrade.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+echo "Requesting root..."
+sudo true
+
+pubkey=$(cat pubkey.txt)
+privkey=$(cat privkey.txt)
+cli="komodo-cli"
+daemon="komodod -notary -pubkey=${pubkey}"
+
+echo "Building latest komodod..."
+cd ~/komodo/ && git checkout dev && git pull && make clean && ./zcutil/build.sh -j$(nproc)
+
+echo "Symlinking latest komodod binary..."
+sudo ln -sf /home/${USER}/komodo/src/komodo-cli /usr/local/bin/komodo-cli
+sudo ln -sf /home/${USER}/komodo/src/komodod /usr/local/bin/komodod
+
+echo "Stopping komodod and assetchains..."
+./ac-cli.sh stop
+${cli} stop
+
+echo "Waiting for them to exit gracefully..."
+sleep 20
+
+echo "Starting komodod and assetchains with new binary..."
+${daemon} > /dev/null 2>&1 &
+sleep 20
+./assetchains.sh > /dev/null 2>&1
+cd ~/VerusCoin/src/ && ./verusd -pubkey=$pubkey > /dev/null 2>&1 &
+sleep 20
+
+echo "Importing privkey..."
+./ac-cli.sh importprivkey $privkey
+
+echo "Updating iguana..."
+cd ~/SuperNET/iguana && git checkout dev && git pull && ./m_notary > ~/logs/iguana 2>&1
+
+echo "Init dPoW..."
+cd ~/komodo/src && ./dpowassets
