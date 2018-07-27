4bebf09faea721b885404adff82b8fa94a982428
Luke Childs <lukechilds123@gmail.com>
2018-07-27 03:38:17 +0100

    Add assetchains.sh

---
+++ assetchains.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -eo pipefail
+
+cli="komodo-cli"
+overide_args="$@"
+pubkey=$(cat pubkey.txt)
+seed_ip=$(getent hosts zero.kolo.supernet.org | awk '{ print $1 }')
+
+./listassetchainparams | while read args; do
+  ${cli} ${args} ${overide_args} -pubkey=${pubkey} -addnode=${seed_ip} &
+done
