8c098e5b57027cc697a2836fb9221ef5990e6034
Luke Childs <lukechilds123@gmail.com>
2018-07-27 03:59:26 +0100

    Add walletresetvrsc.sh

---
+++ walletresetvrsc.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+source ~/komodo/src/pubkey.txt
+
+coin="VRSC"
+daemon="${HOME}/VerusCoin/src/verusd -pubkey=${pubkey}"
+daemon_process_regex="verusd.*\-pubkey"
+cli="komodo-cli -ac_name=VRSC"
+wallet_file="${HOME}/.komodo/VRSC/wallet.dat"
+nn_address="RPxsaGNqTKzPnbm5q7QXwu7b6EZWuLxJG3"
+
+./walletreset.sh \
+  "${coin}" \
+  "${daemon}" \
+  "${daemon_process_regex}" \
+  "${cli}" \
+  "${wallet_file}" \
+  "${nn_address}"
