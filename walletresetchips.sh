02861285ce83b9c4ca636f46df17f6af55cf5844
Luke Childs <lukechilds123@gmail.com>
2018-07-27 05:01:27 +0100

    Add walletresetchips.sh

---
+++ walletresetchips.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+source ~/komodo/src/pubkey.txt
+
+coin="CHIPS"
+daemon="chipsd -pubkey=${pubkey}"
+daemon_process_regex="chipsd.*\-pubkey"
+cli="chips-cli"
+wallet_file="${HOME}/.chips/wallet.dat"
+nn_address="RPxsaGNqTKzPnbm5q7QXwu7b6EZWuLxJG3"
+
+./walletreset.sh \
+  "${coin}" \
+  "${daemon}" \
+  "${daemon_process_regex}" \
+  "${cli}" \
+  "${wallet_file}" \
+  "${nn_address}"
