cff9426c1d4aa001e8e8ad7e1702e2547f039cd9
Luke Childs <lukechilds123@gmail.com>
2018-07-27 03:55:05 +0100

    Add walletresetgame.sh

---
+++ walletresetgame.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+source ~/komodo/src/pubkey.txt
+
+coin="GAME"
+daemon="gamecreditsd -pubkey=${pubkey}"
+daemon_process_regex="gamecreditsd.*\-pubkey"
+cli="gamecredits-cli"
+wallet_file="${HOME}/.gamecredits/wallet.dat"
+nn_address="GYXbuspVqMo7o4hBHt5XH98HF8tmJCbsaH"
+
+./walletreset.sh \
+  "${coin}" \
+  "${daemon}" \
+  "${daemon_process_regex}" \
+  "${cli}" \
+  "${wallet_file}" \
+  "${nn_address}"
