2e300788793e378be0fe3d4db6b80a79821b5b73
Luke Childs <lukechilds123@gmail.com>
2018-10-04 09:51:21 +0700

    Set correct UTXO size in splitfunds.sh for EMC2

--- splitfunds.sh
+++ splitfunds.sh
@@ -8,5 +8,8 @@ utxo_size=10000
 if [[ ${coin} = "GAME" ]]; then
   utxo_size=100000
 fi
+if [[ ${coin} = "EMC2" ]]; then
+  utxo_size=100000
+fi
 
 curl http://127.0.0.1:7776 --silent --data "{\"coin\":\"${coin}\",\"agent\":\"iguana\",\"method\":\"splitfunds\",\"satoshis\":${utxo_size},\"sendflag\":1,\"duplicates\":${duplicates}}"
