d470099d29f7b4e18949ce007fdcbe1a17631b49
Luke Childs <lukechilds123@gmail.com>
2019-04-23 09:37:47 +0700

    Clean HUSH3 wallet with other ACs

--- cleanwallettransactionskmdandacs.sh
+++ cleanwallettransactionskmdandacs.sh
@@ -3,6 +3,10 @@ cd "${BASH_SOURCE%/*}" || exit
 
 ./cleanwallettransactions.sh KMD &
 
+# HUSH3 isn't uses it's own komodod so isn't in assetchains.json
+# we need to call it manually
+./cleanwallettransactions.sh HUSH3 &
+
 ./listassetchains | while read chain; do
   ./cleanwallettransactions.sh ${chain} &
 done
