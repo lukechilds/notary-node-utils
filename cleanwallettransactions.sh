3f22cbf776b1abaf01396a5ef66ceae49a6d4f8e
Luke Childs <lukechilds123@gmail.com>
2019-07-07 13:06:42 +0700

    Update cleanwallettransactions to work with any coin argument

--- cleanwallettransactions.sh
+++ cleanwallettransactions.sh
@@ -4,8 +4,32 @@ cd "${BASH_SOURCE%/*}" || exit
 # Coin we're resetting
 coin=$1
 
+possible_coins="
+KMD
+HUSH3
+$(./listassetchains)
+"
+
+function list_include_item {
+  local list="$1"
+  local item="$2"
+  if [[ $list =~ (^|[[:space:]])"$item"($|[[:space:]]) ]] ; then
+    result=0
+  else
+    result=1
+  fi
+  return $result
+}
+
 if [[ -z "${coin}" ]]; then
   echo "No coin set, can't clean wallet transactions!"
+  exit
+fi
+
+list_include_item "$possible_coins" "$coin"
+result=$?
+if [[ $result = 1 ]]; then
+  echo "[$coin] $(date) | No clean wallet transaction RPC method available for this coin"
   exit
 fi
 
